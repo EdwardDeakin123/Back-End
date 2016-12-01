600d38d2daeda239bc15b7a362e102f0549047ae
Llewellyn <Llewellyn@DESKTOP-FM6CJGQ>
2016-12-01 18:50:19 +1100

    - Base project initial commit.

---
+++ Back-End/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Back_End.Startup))]
+namespace Back_End
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
