f941b67bfecbad974f46ce164e86711bfbb974dd
Llewellyn <Llewellyn@DESKTOP-FM6CJGQ>
2016-12-11 23:35:20 +1100

    - Created an Activity model and started creating an activity controller to implement the API.

---
+++ Back-End/Controllers/ActivityController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Back_End.Models;
+
+namespace Back_End.Controllers
+{
+    public class ActivityController : ApiController
+    {
+        public List<Activity> GetAll()
+        {
+            Activity testAct = new Activity();
+            testAct.ActivityId = 0;
+            testAct.ActivityName = "Running";
+
+            List<Activity> activityList = new List<Activity>();
+            activityList.Add(testAct);
+
+            return activityList;
+        }
+    }
+}
---
+++ Back-End/Models/Activity.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace Back_End.Models
+{
+    public class Activity
+    {
+        public int ActivityId { get; set; }
+        public string ActivityName { get; set; }
+    }
+
+    public class ActivityDbContext : DbContext
+    {
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
