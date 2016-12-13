8b08da36bb2d43fd5679b76ec29658006a8ab1f4
Llewellyn <Llewellyn@DESKTOP-FM6CJGQ>
2016-12-13 19:39:30 +1100

    - Updated comments to include more information. Added the ActivityLog controller. Need to add filtering by date range.

--- Back-End/Controllers/ActivityController.cs
+++ Back-End/Controllers/ActivityController.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using Back_End.Models;
 using Back_End.Database;
 
 namespace Back_End.Controllers
 {
-    // API Controller for Activity Model.
+    /// <summary>
+    /// API Controller for Activity Model.
+    /// </summary>
     // Inheriting from ApiController to implement the API.
     public class ActivityController : ApiController
     {
+        // Access the ActivityTracker database.
         private ActivityTrackerContext _Database = new ActivityTrackerContext();
 
-        // GET: /Activity/GetAll
+        /// <summary>
+        /// GET: /Activity/GetAll
+        /// </summary>
+        /// <returns>A List containing all the activities stored in the database.</returns>
         public List<Activity> GetAll()
         {
-            Activity testAct = new Activity();
-            testAct.ActivityId = 0;
-            testAct.ActivityName = "Running";
-
-            List<Activity> activityList = new List<Activity>();
-            activityList.Add(testAct);
-
             return _Database.Activities.ToList();
         }
 
-        // GET: /Activity/Get
+        /// <summary>
+        /// GET: /Activity/Get
+        /// </summary>
+        /// <param name="activityId">The ID of an activity to get from the database</param>
+        /// <returns>The Activity associated with the ID passed.</returns>
         public Activity Get(int activityId)
         {
             Activity testAct = new Activity();
---
+++ Back-End/Controllers/ActivityLogController.cs
@@ -0,0 +1,44 @@
+﻿using Back_End.Database;
+using Back_End.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Back_End.Controllers
+{
+    /// <summary>
+    /// API Controller for ActivityLog Model.
+    /// </summary>
+    // Inheriting from ApiController to implement the API.
+    public class ActivityLogController : ApiController
+    {
+        // Access the ActivityTracker database.
+        private ActivityTrackerContext _Database = new ActivityTrackerContext();
+
+        /// <summary>
+        /// GET: /ActivityLog/Get
+        /// </summary>
+        /// <param name="activityLogId">The ID of an ActivityLog entry to get from the database</param>
+        /// <returns>The ActivityLog associated with the ID passed.</returns>
+        public ActivityLog Get(int activityLogId)
+        {
+            // Get the element from the database.
+            return _Database.ActivityLogs.SingleOrDefault(log => log.ActivityLogId == activityLogId);
+        }
+
+        /// <summary>
+        /// GET: /ActivityLog/GetByUser
+        /// </summary>
+        /// <param name="userId">The ID of a User</param>
+        /// <returns>A list of activity logs associated with this user.</returns>
+        public List<ActivityLog> GetByUser(int userId)
+        {
+            // Get all elements in the database for this user.
+            // Probably won't be used very much if at all. Should instead filter by date.
+            return _Database.ActivityLogs.Where(log => log.User.UserId == userId).ToList();
+        }
+    }
+}
