39592f1021dfc215d42015bb6dfee1faf367c8fb
Llewellyn <Llewellyn@DESKTOP-FM6CJGQ>
2016-12-15 15:17:51 +1100

    - Added the intial implementation of user registration.

---
+++ Back-End/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Back_End.Database;
+using Back_End.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Back_End.Controllers
+{
+    public class UserController : ApiController
+    {
+        // Access the ActivityTracker database.
+        private ActivityTrackerContext _Database = new ActivityTrackerContext();
+
+        // POST: /User/Register
+        [HttpPost]
+        public void Register(string firstname, string lastname, string username, string password)
+        {
+            // Create the new User object.
+            //TODO: Encrypt the password.
+            //TODO: Add a salt to the password.
+            //TODO: Check for a user with this username before adding it to the database.
+            User newUser = new User { FirstName = firstname, LastName = lastname, Username = username, Password = password };
+
+            // Add the user to the database.
+            _Database.Users.Add(newUser);
+        }
+    }
+}
