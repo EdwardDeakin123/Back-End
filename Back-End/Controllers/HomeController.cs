ce18c9fe1bcbe0e20531df7295e322ddd51e4803
EdwardDeakin123 <eplumrid@deakin.edu.au>
2017-02-02 15:02:14 +1100

    The excel export

--- Back-End/Controllers/HomeController.cs
+++ Back-End/Controllers/HomeController.cs
@@ -8,10 +8,45 @@ namespace Back_End.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+       private ActivityTrackerContext _Database = new ActivityTrackerContext();
+
+       
+        
+        public List<ActivityLog> GetAll2()
+        {
+            return _Database.ActivityLogs.ToList();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(GetAll2());
+        }
+
+        public void ExportClientsListToCSV()
+        {
+
+            StringWriter sw = new StringWriter();
+            
+
+            sw.WriteLine("\"Start Time\",\"End Time\",\"User\",\"Activity\"");
+
+            Response.ClearContent();
+            Response.AddHeader("content-disposition", "attachment;filename=Exported_Users.csv");
+            Response.ContentType = "text/csv";
+
+            foreach (var line in GetAll2())
+            {
+                sw.WriteLine(string.Format("\"{0}\",\"{1}\",\"{2}\",\"{3}\"",
+                                           line.StartTime,
+                                           line.EndTime,
+                                           line.User.Username,
+                                           line.Activity.ActivityName));
+            }
+
+            Response.Write(sw.ToString());
+
+            Response.End();
+
         }
     }
 }
